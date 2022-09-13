8224c8403328e64ac7c45ae7945745e2d6cf386c
MiccWan <michaelwan0906@hotmail.com>
2022-09-13 12:01:52 +0800

    better logging

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import Koa from 'koa';
+import { v4 as uuidv4 } from 'uuid';
+
 const app = new Koa();
 
+let i = 0;
+const uid = uuidv4();
+
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  ctx.body = `Hello World\nThis is the ${i++}-th connection to:\n${uid}`;
   ctx.status = 200;
 });
 
