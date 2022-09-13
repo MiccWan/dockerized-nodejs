f731a9821aec89a31c5f03a0fa353242d86d6c77
MiccWan <michaelwan0906@hotmail.com>
2022-09-13 15:02:48 +0800

    higher app load

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ let i = 0;
 const uid = uuidv4();
 
 app.use(async ctx => {
-  ctx.body = `Hello World\nThis is the ${i++}-th connection to:\n${uid}\n\n`;
+  let arr = [];
+  for (let j = 0; j < 100000; j++) {
+    arr.push(Math.random());
+  }
+  ctx.body = `Hello World\nThis is the ${i++}-th connection to:\n${uid}\nrandom sum: ${arr.reduce((a, b) => a + b, 0)}\n\n`;
   ctx.status = 200;
 });
 
